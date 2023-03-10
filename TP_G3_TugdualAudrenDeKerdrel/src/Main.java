0fb15da8ff96f1bd6481ac6f42b75c82063d5c67
Tugdual de K <tugdualk@hotmail.com>
2023-03-10 14:29:11 +0100

    end tp3 safe scanner + ascii

--- TP_G3_TugdualAudrenDeKerdrel/src/Main.java
+++ TP_G3_TugdualAudrenDeKerdrel/src/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println("--- Troisieme TP ---");
         TroisiemTp tp3 = new TroisiemTp();
         tp3.testScanner();
+        tp3.drawCat(5, 7);
     }
 }
--- TP_G3_TugdualAudrenDeKerdrel/src/TroisiemTp.java
+++ TP_G3_TugdualAudrenDeKerdrel/src/TroisiemTp.java
@@ -1,6 +1,6 @@
 public class TroisiemTp {
 
-    public void testScanner(){
+    public void testScanner() {
         SafeScanner scanner = new SafeScanner();
 
         System.out.println("Comment vous appelez vous ?");
@@ -12,5 +12,40 @@ public class TroisiemTp {
         System.out.println("Votre age est " + age);
 
         scanner.closeScanner();
+    }
+
+    public void drawCat(int height, int width) {
+
+        // ears and top of the head
+        System.out.print("|\\");
+        printCharLoop(width - 4, '-');
+        System.out.println("/|");
+
+        // middle of the head and eye
+        for (int i = 1; i < height - 1; i++) {
+            System.out.print("|");
+            if (i == height / 2) {
+                System.out.print(" ");
+                System.out.print("0");
+                printCharLoop(width - 6, '_');
+                System.out.print("0");
+                System.out.print(" ");
+            } else {
+                printCharLoop(width - 2, ' ');
+            }
+            System.out.println("|");
+        }
+
+        // bottom of the head
+        System.out.print(" \\_");
+        printCharLoop(width - 6, '^');
+        System.out.println("_/ ");
+    }
+
+    private void printCharLoop(int n, char c) {
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(c);
+        }
     }
 }
